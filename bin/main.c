45191990802a49641cb569b6b97bc3b6f98b0e7d
Navaliz <51031536+Navaliz@users.noreply.github.com>
2020-02-02 23:37:29 +0700

    [Added] Inventory
    inventory added

--- bin/main.c
+++ bin/main.c
@@ -7,25 +7,33 @@
 
 void play();
 int main(){
-    
+
+
     newdisp();
     nameinput();
     system("pause");
-    
+
     play();
-    
+
     printf("\nYou survived for %d Day and %.f hour.\n\n", day, player.time);
     system("pause");
-    
+
     return 0;
 }
 
 void play(){
-    newdisp2();//title, time, player stats.
+    newdisp1();
+    printstats(&player);
     while(player.hp > 0){
+        int act = 0;
+        printf("\n\n#Inventory:\n  Spinner\t: %d\n  Rubik Cube\t: %d\n  RC Car\t: %d\n\n",spinner,rubikCube,rcCar);
+        actionlist();
+        printf("\nChoose the action you want to do: ");
+
+        scanf("%d", &act);
+        action(act);
+        newdisp1();
         timecheck();
-        doact();
-        newdisp2();
-        timecheck();
-    }       
-} 
+        printstats(&player);
+    }
+}
