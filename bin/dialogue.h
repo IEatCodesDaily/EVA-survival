305acc34772163db272928dc68f7188cba4e6abe
Navaliz <51031536+Navaliz@users.noreply.github.com>
2020-02-02 23:42:35 +0700

    [Added] new variable and following the prev update
    new var
    update

--- bin/dialogue.h
+++ bin/dialogue.h
@@ -1,30 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//init other
+float money;
+char next;
+int p;
+//init item
+int spinner=0;
+int rubikCube=0;
+int rcCar=0;
 
 void actionlist(){
-    printf("\n\nWhat should i do?");
+    printf("\nWhat should i do?");
     int i = 1;
     while (actlib[i][0] != '\0'){
         printf("\n%d. %s", i, actlib[i]);
         i++;
     }
-    
+
+}
+
+void worklist(){
+    printf("\nWhat should i do?\n");
+    int i = 1;
+    while (worklib[i][0] != '\0'){
+        printf("\n%d. %s", i, worklib[i]);
+        i++;
+    }
+
 }
 
 void joblist(){
-    printf("\n\nWhat job should i take?");
+    printf("\n What job should i take?");
     int i = 1;
     while (worklib[i][0] != '\0'){
-        printf("\n%d. %s\t\t\t\tSalary : $%d\tDuration : %d hour(s)", i, worklib[i], salary[i], worktime[i]);
+        printf("\n%d. %s\t\t\t\tSalary : $%.f\tDuration : %.f hour", i, worklib[i], salary[i], worktime[i]);
         i++;
     }
 }
 
-void actdial(struct stats *act){
-
-}
-
-void workdial(struct work *work){
+void sleepdial(){
 
 }
